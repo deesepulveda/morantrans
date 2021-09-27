5a45e643674eb328d694b41317ebf8b5147166c3
dee chao <chaoserady@gmail.com>
2021-09-27 06:44:12 -0500

    added header, title, jumbo, burger

--- app.js/script.js
+++ app.js/script.js
@@ -3,4 +3,54 @@
 
 // ------------------------------
 
-console.log("hello");
+const navBar = document.getElementById("navbar");
+const burger = document.getElementById("burger");
+const topBun = document.getElementById("top");
+const meatBun = document.getElementById("meat");
+const bottomBun = document.getElementById("bottom");
+
+// ------------------------------
+
+function navOpen() {
+  navBar.classList.toggle("showNav");
+  listOne.classList.toggle("slideLeft-One");
+  listTwo.classList.toggle("slideLeft-Two");
+  listThree.classList.toggle("slideLeft-Three");
+  listFour.classList.toggle("slideLeft-Four");
+  topBun.classList.toggle("topbun-change"); //ROTATE TOP LINE
+  meatBun.classList.toggle("middle-change"); //ROTATE MIDDLE LINE
+  bottomBun.classList.toggle("bottombun-change"); //ROTATE BOTTOM LINE
+}
+
+// CLICK TO SHOW NAV MENU AND ANIMATIONS ----
+
+burger.addEventListener("click", navOpen);
+
+// ------------------------------
+
+const trackLink = document.getElementById("track-link");
+const loginLink = document.getElementById("login-link");
+const loginModal = document.getElementById("login-modal");
+const trackModal = document.getElementById("track-modal");
+const trackBox = document.getElementById("track-box");
+const loginBox = document.getElementById("login-box");
+const trackText = document.getElementById("trackText");
+const loginText = document.getElementById("loginText");
+const trackClose = document.getElementById("trackClose");
+const loginClose = document.getElementById("loginClose");
+
+loginLink.addEventListener("click", () => {
+  loginModal.classList.toggle("showModal");
+  loginLink.classList.toggle("btnModalEffects");
+  trackBox.classList.toggle("indexMove");
+  loginClose.classList.toggle("showX");
+  loginText.classList.toggle("loginTextClose");
+});
+
+trackLink.addEventListener("click", () => {
+  trackModal.classList.toggle("showModal");
+  trackLink.classList.toggle("btnModalEffects");
+  loginBox.classList.toggle("indexMove");
+  trackClose.classList.toggle("showX");
+  trackText.classList.toggle("loginTextClose");
+});
